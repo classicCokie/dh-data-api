45b480cc66db6ef109419951f53b1b818ed0d73f
yannic <y.duckwitz@googlemail.com>
2020-12-13 21:06:59 +0100

    feat: added koa

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # dh-data-api
+
+## How to run
+
+```
+npm i
+```
+
+```
+npm run start:dev
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello Baby");
+import Koa from "koa";
+const app = new Koa();
+
+app.use(async (ctx) => {
+  ctx.body = "Hello World";
+});
+
+console.log("Server is listenening on 3000");
+app.listen(3000);
