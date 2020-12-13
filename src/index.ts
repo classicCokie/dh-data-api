9e3fe3da77e65bda28eec66c60040a48ba682e90
yannic <y.duckwitz@googlemail.com>
2020-12-13 21:20:05 +0100

    feat: added basic koa setup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import Koa from "koa";
-const app = new Koa();
+import Router from "@koa/router";
+import BodyParser from "koa-bodyparser";
 
-app.use(async (ctx) => {
-  ctx.body = "Hello World";
+const app = new Koa();
+let router = new Router();
+
+router.get("/hello-world", async (ctx, next) => {
+  ctx.body = "Toll!";
 });
+
+app.use(BodyParser()).use(router.routes()).use(router.allowedMethods());
 
 console.log("Server is listenening on 3000");
 app.listen(3000);
