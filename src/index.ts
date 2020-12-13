4a576cc21e4ec5da48a3b9b26ba39b04154bde05
yannic <y.duckwitz@googlemail.com>
2020-12-13 21:44:38 +0100

    feat: added basic error handeling

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,11 @@ router.get("/hello-world", async (ctx: Koa.BaseContext) => {
     ctx.body = "Toll!";
 });
 
+// Handle errors
+app.on("error", (err) => {
+    console.error("server error", err);
+});
+
 app.use(BodyParser()).use(router.routes()).use(router.allowedMethods());
 
 console.log("Server is listenening on 3000");
