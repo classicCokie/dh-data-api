01bcfa85b45a125182c4b0bf7f9805bf3bd12a43
yannic <y.duckwitz@googlemail.com>
2020-12-20 12:42:22 +0100

    fix: removed healthcheck test

--- test/integration/user.spec.ts
+++ test/integration/user.spec.ts
@@ -13,7 +13,7 @@ chai.use(chaiHttp);
 describe("User Integration Test Suite", function () {
     it("Should Error on Trying to Register User with no data", function (done) {
         chai.request(server)
-            .post("/healthcheck")
+            .post("/user")
             .send({})
             .end(function (err, res) {
                 expect(res).to.have.status(200);
